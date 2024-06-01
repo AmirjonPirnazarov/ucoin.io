8562a97a4339c5765f1fbdbdc469b1a9dcf81285
AmirjonPirnazarov <86454687+AmirjonPirnazarov@users.noreply.github.com>
2024-06-02 01:26:26 +0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,31 +7,29 @@ let total = localStorage.getItem('total');
 let power = localStorage.getItem('power');
 let count = localStorage.getItem('count')
 
-if(coins == null){
-    localStorage.setItem('coins' , '0');
+if (coins == null) {
+    localStorage.setItem('coins', '0');
     h1.textContent = '0';
-}else{
+} else {
     h1.textContent = Number(coins).toLocaleString();
 }
 
-if(total == null){
-    localStorage.setItem('total' , '500')
+if (total == null) {
+    localStorage.setItem('total', '500')
     body.querySelector('#total').textContent = '/500';
-}else {
+} else {
     body.querySelector('#total').textContent = `/${total}`;
 }
 
-
-if(power == null){
-    localStorage.setItem('power' , '500');
+if (power == null) {
+    localStorage.setItem('power', '500');
     body.querySelector('#power').textContent = '500';
-}else{
+} else {
     body.querySelector('#power').textContent = power;
 }
 
-
-if(count == null){
-    localStorage.setItem('count' , '1')
+if (count == null) {
+    localStorage.setItem('count', '1')
 }
 
 const handleClick = (e) => {
@@ -46,47 +44,47 @@ const handleClick = (e) => {
         y = touch.clientY - rect.top;
     }
 
-    navigator.vibrate(5);
+    // Убедитесь, что это выполняется после взаимодействия пользователя
+    if (navigator.vibrate) {
+        navigator.vibrate(5);
+    }
 
     coins = localStorage.getItem('coins');
     power = localStorage.getItem('power');
     
-    if(Number(power) > 0){
-        localStorage.setItem('coins' , `${Number(coins) + 1}`);
+    if (Number(power) > 0) {
+        localStorage.setItem('coins', `${Number(coins) + 1}`);
         h1.textContent = `${(Number(coins) + 1).toLocaleString()}`;
     
-        localStorage.setItem('power' , `${Number(power) - 1}`);
+        localStorage.setItem('power', `${Number(power) - 1}`);
         body.querySelector('#power').textContent = `${Number(power) - 1}`;
-    } 
+    }
 
-    if(x < 150 && y < 150){
+    if (x < 150 && y < 150) {
         image.style.transform = 'translate(-0.25rem, -0.25rem) skewY(-10deg) skewX(5deg)';
-    }
-    else if (x < 150 && y > 150){
+    } else if (x < 150 && y > 150) {
         image.style.transform = 'translate(-0.25rem, 0.25rem) skewY(-10deg) skewX(5deg)';
-    }
-    else if (x > 150 && y > 150){
+    } else if (x > 150 && y > 150) {
         image.style.transform = 'translate(0.25rem, 0.25rem) skewY(10deg) skewX(-5deg)';
-    }
-    else if (x > 150 && y < 150){
+    } else if (x > 150 && y < 150) {
         image.style.transform = 'translate(0.25rem, -0.25rem) skewY(10deg) skewX(-5deg)';
     }
 
-    setTimeout(()=>{
+    setTimeout(() => {
         image.style.transform = 'translate(0px, 0px)';
     }, 100);
 
     body.querySelector('.progress').style.width = `${(100 * power) / total}%`;
 };
 
-image.addEventListener('click', handleClick);
-image.addEventListener('touchstart', handleClick);
+image.addEventListener('click', handleClick, { passive: true });
+image.addEventListener('touchstart', handleClick, { passive: true });
 
-setInterval(()=> {
-    count = localStorage.getItem('count')
+setInterval(() => {
+    count = localStorage.getItem('count');
     power = localStorage.getItem('power');
-    if(Number(total) > power){
-        localStorage.setItem('power' , `${Number(power) + Number(count)}`);
+    if (Number(total) > power) {
+        localStorage.setItem('power', `${Number(power) + Number(count)}`);
         body.querySelector('#power').textContent = `${Number(power) + Number(count)}`;
         body.querySelector('.progress').style.width = `${(100 * power) / total}%`;
     }
